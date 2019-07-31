998e97f45708fe1ff8a0a52c3d51101dcfb41660
Paul Abernathy <paul.abernathy7@gmail.com>
2019-07-31 19:38:26 -0400

    post comments work

--- data/db-router.js
+++ data/db-router.js
@@ -22,13 +22,39 @@ router.post("/", (req, res) => {
       .catch(err => {
         res.status(500).json({
           error: "There was an error while saving the post to the database",
-          message: err.message
+          message: err.message // always include err.message
         });
       });
   }
 });
 
-router.post("/:id/comments", (req, res) => {});
+router.post("/:id/comments", (req, res) => {
+  const { id } = req.params;
+  const text = req.body;
+
+  if (!id) {
+    res
+      .status(404)
+      .json({ message: "The post with the specified ID does not exist." });
+  } else {
+    Posts.insertComment(text)
+      .then(post => {
+        if (post) {
+          res.status(201).json(post);
+        } else {
+          res
+            .status(400)
+            .json({ errorMessage: "Please provide text for the comment." });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: "There was an error while saving the comment to the database",
+          err: err.message // always include err.message
+        });
+      });
+  }
+});
 
 module.exports = router;
 // server.use("/api/posts", postsRouter);
