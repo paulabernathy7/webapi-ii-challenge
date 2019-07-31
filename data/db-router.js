e97a58ac7d07de02a0ec9ab37c9f49e8a184cb06
Paul Abernathy <paul.abernathy7@gmail.com>
2019-07-30 23:26:35 -0400

    sometime is wrong here

--- data/db-router.js
+++ data/db-router.js
@@ -1,5 +1,33 @@
 const express = require("express");
-const Data = require("./db");
+const Posts = require("./db");
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.send({ success: " ready" });
+});
+
+router.post("/", (req, res) => {
+  const { title, contents } = req.body;
+  console.log(req.body, "api");
+
+  if (!title || !contents) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    });
+  } else {
+    Posts.insert(req.body)
+      .then(post => {
+        res.status(201), json(post);
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: "There was an error while saving the post to the database"
+        });
+      });
+  }
+});
+
+router.post("/:id/comments", (req, res) => {});
+
 module.exports = router;
+// server.use("/api/posts", postsRouter);
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
-const dataRouter = require("./data/db-router");
+const dataRouter = require("./data/db-router"); //importing db-router file
 
 const server = express(); // other req parsers are available, this is also called middleware
 server.use(express.json());
-server.use("/api/posts", dataRouter);
+server.use("/api/posts", dataRouter); // allows me to use the db-router for routes
+
+//base URL
+server.get("/", (req, res) => {
+  res.send({ success: "We're ready" });
+});
 
 module.exports = server;
