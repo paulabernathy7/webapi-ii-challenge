79b33b3fce0e3f22c24f20a8f676a633f602c422
Paul Abernathy <paul.abernathy7@gmail.com>
2019-07-31 21:05:24 -0400

    GET comments works

--- data/db-router.js
+++ data/db-router.js
@@ -89,5 +89,25 @@ router.get("/:id", (req, res) => {
     });
 });
 
+router.get("/:id/comments", (req, res) => {
+  const { id } = req.params;
+  Posts.findPostComments(id)
+    .then(posts => {
+      if (posts) {
+        res.status(200).json(posts);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "The comments information could not be retrieved.",
+        err: err.message
+      });
+    });
+});
+
 module.exports = router;
 // server.use("/api/posts", postsRouter);
