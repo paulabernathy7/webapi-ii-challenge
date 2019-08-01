a7efb3b7cf60a7ce91e8019fbe3af3cd538bfeda
Paul Abernathy <paul.abernathy7@gmail.com>
2019-07-31 20:28:21 -0400

    GET posts works

--- data/db-router.js
+++ data/db-router.js
@@ -69,5 +69,25 @@ router.get("/", (req, res) => {
         .json({ error: "The posts information could not be retrieved." });
     });
 });
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  Posts.findById(id)
+    .then(posts => {
+      if (posts) {
+        res.status(200).json(posts);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The post information could not be retrieved." });
+    });
+});
+
 module.exports = router;
 // server.use("/api/posts", postsRouter);
