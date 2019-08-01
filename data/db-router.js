63098dd677b7b207944f34a33c8c517de9adc393
Paul Abernathy <paul.abernathy7@gmail.com>
2019-07-31 20:07:30 -0400

    first GET request done

--- data/db-router.js
+++ data/db-router.js
@@ -2,9 +2,11 @@ const express = require("express");
 const Posts = require("./db");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send({ success: " ready" });
-});
+//THIS FILE HANDLES ROUTES
+
+// router.get("/", (req, res) => {
+//   res.send({ success: " ready" });
+// });
 
 router.post("/", (req, res) => {
   const { title, contents } = req.body;
@@ -56,5 +58,16 @@ router.post("/:id/comments", (req, res) => {
   }
 });
 
+router.get("/", (req, res) => {
+  Posts.find()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The posts information could not be retrieved." });
+    });
+});
 module.exports = router;
 // server.use("/api/posts", postsRouter);
