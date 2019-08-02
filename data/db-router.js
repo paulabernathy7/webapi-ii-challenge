b3566cc8778ea2b499a6177f3da6a36cdd98fd10
Paul Abernathy <paul.abernathy7@gmail.com>
2019-08-01 21:43:43 -0400

    put works

--- data/db-router.js
+++ data/db-router.js
@@ -126,5 +126,32 @@ router.delete("/:id", (req, res) => {
       res.status(500).json({ error: "The post could not be removed" });
     });
 });
+
+router.put("/:id", (req, res) => {
+  const { title, contents } = req.body;
+  const { id } = req.params;
+
+  if (!title || !contents) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    });
+  } else {
+    Posts.update(id, req.body)
+      .then(posts => {
+        if (posts) {
+          res.status(200).json(posts);
+        } else {
+          res.status(404).json({
+            message: "The post with the specified ID does not exist."
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({
+          errorMessage: "The post information could not be modified."
+        });
+      });
+  }
+});
 module.exports = router;
 // server.use("/api/posts", postsRouter);
